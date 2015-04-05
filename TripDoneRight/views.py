524f06f0a956e3b1a297a1d27bc1a2371813b855
Natan Lailari <natan.lailari@gmail.com>
2015-04-05 18:54:49 -0400

    app routes

--- TripDoneRight/views.py
+++ TripDoneRight/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango's gonna say hey there world!")
 # Create your views here.
